scaling in barrier net fix

code/examples/run_singleintegrator.py
@@ -4,6 +4,7 @@ from run import run, parse_args
 from sim import run_sim
 from systems.singleintegrator import SingleIntegrator
 from other_policy import APF
+import plotter 
 
 # standard
 from torch import nn, tanh, relu
@@ -35,6 +36,17 @@ class SingleIntegratorParam(Param):
 		self.a_min = -1*self.a_max
 		self.D_robot = 1.1*(self.r_agent+self.r_agent)
 		self.D_obstacle = 1.1*(self.r_agent + self.r_obstacle)
+		self.circle_obstacles_on = True # square obstacles batch not implemented 		
+
+		self.max_neighbors = 5
+		self.max_obstacles = 5
+		# Barrier function stuff
+		self.b_gamma = 0.01 # 0.1
+		self.b_exph = 1.0 # 1.0
+		# cbf 
+		self.cbf_kp = 0.2
+		self.cbf_kv = 1.5
+		self.a_noise = 0.002
 
 		# 
 		self.phi_max = self.a_max
@@ -42,7 +54,7 @@ class SingleIntegratorParam(Param):
 		
 		# sim 
 		self.sim_t0 = 0
-		self.sim_tf = 100
+		self.sim_tf = 25
 		self.sim_dt = 0.1
 		self.sim_times = np.arange(self.sim_t0,self.sim_tf,self.sim_dt)
 		self.sim_nt = len(self.sim_times)
@@ -55,9 +67,9 @@ class SingleIntegratorParam(Param):
 		self.il_test_train_ratio = 0.8
 		self.il_batch_size = 5000
 		self.il_n_epoch = 5000
-		self.il_lr = 5e-3
+		self.il_lr = 1e-3
 		self.il_wd = 0*0.01
-		self.il_n_data = 100000
+		self.il_n_data = 10000
 		self.il_log_interval = 1
 		self.il_load_dataset = ['orca','centralplanner'] # 'random','ring','centralplanner'
 		self.il_controller_class = 'Barrier' # 'Empty','Barrier'
@@ -93,22 +105,10 @@ class SingleIntegratorParam(Param):
 
 		self.il_network_activation = relu
 
-		self.max_neighbors = 3
-		self.max_obstacles = 3
-
 		# Sim
 		self.sim_rl_model_fn = '../models/singleintegrator/rl_current.pt'
 		self.sim_il_model_fn = '../models/singleintegrator/il_current.pt'
 		self.sim_times = np.arange(self.sim_t0,self.sim_tf,self.sim_dt)
-
-		# Barrier function stuff
-		self.b_gamma = 0.1
-		self.b_exph = 1.0
-		# cbf 
-		self.cbf_kp = 0.2
-		self.cbf_kv = 1.5
-		self.cbf_noise = 0.075
-		self.a_noise = 0.075
 
 
 if __name__ == '__main__':
@@ -122,6 +122,15 @@ if __name__ == '__main__':
 
 	set_ic_on = True 
 	ring_ex_on = False
+
+
+	# TEMP
+	# param = SingleIntegratorParam()
+	# env = SingleIntegrator(param)			
+	# plotter.plot_barrier_fnc(env)
+	# plotter.save_figs(param.plots_fn)
+	# plotter.open_figs(param.plots_fn)
+	# exit()	
 
 	if set_ic_on:
 
@@ -145,14 +154,13 @@ if __name__ == '__main__':
 		else:
 
 			import yaml
-			ex = 2
+			ex = 9 # 4 is hard 
 			
 			if args.instance:
 				with open(args.instance) as map_file:
 					map_data = yaml.load(map_file)
 			else:
 				# test 2 example 
-				# param.n_agents = 2 
 				# with open("../baseline/centralized-planner/examples/test_2_agents.yaml") as map_file:
 
 				# test empty 
@@ -160,6 +168,8 @@ if __name__ == '__main__':
 
 				# test map 
 				with open("../baseline/centralized-planner/examples/map_8by8_obst12_agents10_ex{}.yaml".format(ex)) as map_file:
+				
+
 					map_data = yaml.load(map_file)
 
 			s = []

code/learning/barrier_net.py
@@ -10,7 +10,7 @@ import numpy as np
 
 # my package
 from learning.deepset import DeepSet
-
+from utilities import torch_tile
 
 # standard package
 import gym
@@ -58,6 +58,7 @@ class Barrier_Net(nn.Module):
 		self.phi_min = param.phi_min
 		self.phi_max = param.phi_max
 		self.a_noise = param.a_noise
+		self.circle_obstacles_on = param.circle_obstacles_on
 
 	def policy(self,x):
 
@@ -93,8 +94,6 @@ class Barrier_Net(nn.Module):
 
 	def __call__(self,x):
 
-		
-
 		# temp
 		# nd = 2
 		# x = x[0:nd,:]
@@ -125,23 +124,48 @@ class Barrier_Net(nn.Module):
 		# print('Obstacles')
 		for j in range(no):
 			idx = 1+self.state_dim_per_agent*(nn+1)+j*2+np.arange(0,2,dtype=int)
-			P_i = -1*x[:,idx].numpy() # in nd x state_dim_per_agent
-			A_i = self.get_obstacle_barrier_2(P_i)
+			if self.circle_obstacles_on: 
+				P_i = -1*x[:,idx].numpy() # in nd x state_dim_per_agent
+				A_i = self.get_obstacle_barrier_2(P_i)
+			else:
+				# not implemented yet 
+				A_i = torch.zeros((len(x),self.action_dim_per_agent))
 			barrier_action += torch.from_numpy(A_i).float()
 
 		# 	print('j: ', j)
 		# 	print('P_i: ', P_i)
 		# 	print('A_i: ', A_i)
-
 		# print('barrier_action: ', barrier_action)
 
-		# exit()
 
-		# scale actions 
+		# make final action 
 		action = empty_action + barrier_action 
-		action = action + torch.from_numpy(0.05 * np.random.normal(size=action.shape)).float()
-		action = torch.tanh(action) # action \in [-1,1]
-		action = (action+1.)/2.*torch.tensor((self.a_max-self.a_min)).float()+torch.tensor((self.a_min)).float() # action \in [amin,amax]
+
+		# add noise 
+		action = action + torch.from_numpy(self.a_noise * np.random.normal(size=action.shape)).float()
+		
+		# scale actions 
+		if False:
+			action = torch.tanh(action) # action \in [-1,1]
+			action = (action+1.)/2.*torch.tensor((self.a_max-self.a_min)).float()+torch.tensor((self.a_min)).float() # action \in [amin,amax]
+		else:
+
+			# print('action:', action)
+			# print('action.shape: ', action.shape)
+			# print('action.norm(p=2,dim=1): ', action.norm(p=2,dim=1))
+
+			inv_alpha = action.norm(p=float('inf'),dim=1)/self.a_max 
+			# inv_alpha = torch.max(action,1)[0]/self.a_max
+			inv_alpha = torch.clamp(inv_alpha,min=1)
+			inv_alpha = inv_alpha.unsqueeze(0).T
+			inv_alpha = torch_tile(inv_alpha,1,2)
+			action = action*inv_alpha.pow_(-1)
+
+
+			# print('action:', action)
+			# print('action.shape: ', action.shape)
+			# print('action.norm(p=2,dim=1): ', action.norm(p=2,dim=1))
+
 		return action 
 
 	def get_robot_barrier_2(self,P):
@@ -176,21 +200,66 @@ class Barrier_Net(nn.Module):
 
 	def get_obstacle_barrier(self,dp):
 		norm_p = np.linalg.norm(dp)
-		h_ij = norm_p - self.D_obstacle
+
+		if self.circle_obstacles_on:
+			h_ij = norm_p - self.D_obstacle
+
+		else:
+
+			# dp = pi - pj 
+			# shift coordinate st pj = [0,0] (center of square obstacle)
+			# line_to_agent: line from pj to pi
+			# obstacle boundaries: [0,0] +- r_obstacle*[1,1] 
+			# d1 is the length from center of square to the intersection point on the square
+			# d2 is the length from the agent boundary to the d1
+			# norm p is the length from the center of the agent to the center of the obstacle 
+
+			def line(p1, p2):
+				A = (p1[1] - p2[1])
+				B = (p2[0] - p1[0])
+				C = (p1[0]*p2[1] - p2[0]*p1[1])
+				return A, B, -C
+
+			def intersection(L1, L2):
+				D  = L1[0] * L2[1] - L1[1] * L2[0]
+				Dx = L1[2] * L2[1] - L1[1] * L2[2]
+				Dy = L1[0] * L2[2] - L1[2] * L2[0]
+				if D != 0:
+					x = Dx / D
+					y = Dy / D
+					return np.array([x,y])
+				else:
+					return np.array([False,False])
+
+			dp = dp.numpy()
+			line_to_agent = line([0,0],[dp[0],dp[1]])
+
+			square_lines = []
+			square_lines.append(line(
+				[0-self.r_obstacle,0-self.r_obstacle],
+				[0-self.r_obstacle,0+self.r_obstacle]))
+			square_lines.append(line(
+				[0-self.r_obstacle,0+self.r_obstacle],
+				[0+self.r_obstacle,0+self.r_obstacle]))
+			square_lines.append(line(
+				[0+self.r_obstacle,0+self.r_obstacle],
+				[0+self.r_obstacle,0-self.r_obstacle]))
+			square_lines.append(line(
+				[0+self.r_obstacle,0-self.r_obstacle],
+				[0-self.r_obstacle,0-self.r_obstacle]))
+
+			d_square_to_intersection = np.inf 
+			for square_line in square_lines:
+				r = intersection(line_to_agent,square_line)
+				norm_r = np.linalg.norm(r)
+
+				if r.any() and norm_r < d_square_to_intersection:
+					d_square_to_intersection = norm_r 
+
+			h_ij = np.linalg.norm(dp) - d_square_to_intersection - self.r_agent
+
+
 		return self.b_gamma/np.power(h_ij,self.b_exph)*dp/norm_p
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 

code/plotter.py
@@ -108,6 +108,28 @@ def plot_square(x,y,r,angle=None,fig=None,ax=None,title=None,label=None,color=No
 	return fig,ax	
 	
 
+def plot_barrier_fnc(env):
+	
+	def make_barrier_plot(rsafe,rsense,title=None):
+		fig,ax = make_fig()
+		d = np.linspace(1.001*rsafe,1.1*rsense,100)
+		h = d - rsafe
+		b = np.multiply(d < rsense, env.b_gamma*np.power(h,-1*env.b_exph))
+		ax.plot(d,b)
+		ax.axvline(rsafe,color='red')
+		ax.axvline(rsense,color='orange')
+		ax.set_title(title)
+		ax.set_yscale('log')
+
+	rsafe_agents = env.r_agent*2
+	rsense_agents = env.r_comm 
+	rsafe_obstacles = env.r_agent + env.r_obstacle
+	rsense_obstacles = env.r_obs_sense
+
+	make_barrier_plot(rsafe_agents, rsense_agents, title='Agent Barrier')
+	make_barrier_plot(rsafe_obstacles, rsense_obstacles, title='Obstacle Barrier')
+
+
 def plot_ss(env,states):
 	fig,ax = env.render()
 	ax.plot(states[:,0],states[:,1],linestyle='dashed')
@@ -115,8 +137,5 @@ def plot_ss(env,states):
 
 # def debug_plot_batch(batch):
 	# batch = [o_lst,c_lst,r_lst,op_lst,p_lst,d_lst,s_lst,sp_lst]
-
-	
-
 
 

code/utilities.py
@@ -1,7 +1,7 @@
 
 
 import numpy as np 
-
+import torch 
 
 def to_cvec(x):
 	return np.reshape(x,(len(x),-1))
@@ -52,3 +52,11 @@ def debug_lst(variable_lst):
 		debug(variable)
 
 
+def torch_tile(a, dim, n_tile):
+	# from https://discuss.pytorch.org/t/how-to-tile-a-tensor/13853/4
+    init_dim = a.size(dim)
+    repeat_idx = [1] * a.dim()
+    repeat_idx[dim] = n_tile
+    a = a.repeat(*(repeat_idx))
+    order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)]))
+    return torch.index_select(a, dim, order_index)