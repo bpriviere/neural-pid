7fe9c31f0fa8beaf4adb7343c0b65d6132d7c699
bpriviere <benjamin.p.riviere@gmail.com>
2020-02-19 18:37:49 -0800

    some di param

--- code/examples/run_doubleintegrator.py
+++ code/examples/run_doubleintegrator.py
@@ -33,43 +33,45 @@ class DoubleIntegratorParam(Param):
 		self.r_agent = 0.15 #0.2
 		self.r_obstacle = 0.5
 		self.v_max = 0.5
-		self.a_max = 2.0
-		# self.v_max = np.inf
-		# self.a_max = np.inf 
+		self.a_max = 7.5 # 7.5
+		# self.v_max = 100 
+		# self.a_max = 100
 		self.v_min = -1*self.v_max
 		self.a_min = -1*self.a_max
 
 		# sim 
 		self.sim_t0 = 0
 		self.sim_tf = 100
-		self.sim_dt = 0.025
+		self.sim_dt = 0.05
 		self.sim_times = np.arange(self.sim_t0,self.sim_tf,self.sim_dt)
 		self.sim_nt = len(self.sim_times)
 		self.plots_fn = 'plots.pdf'
 
-		# safety
-		# self.Delta_R = 2*(self.v_max*self.sim_dt + 1/2*self.a_max*self.sim_dt**2) + 1e-6
-		# first term accounts for discretization; second term for breaking distance
-		self.Delta_R = 2*self.v_max*self.sim_dt + self.v_max**2 / (2 * self.a_max)
-
-		# self.Delta_R *= 2
-
 		self.max_neighbors = 6
 		self.max_obstacles = 6
-		
-		# cbf 
-		self.cbf_kp = 0.5
-		self.cbf_kd = 2.0
-		
-		self.pi_max = 0.05 #0.5 #0.5 #0.10 #1.0*self.a_max
-		self.sigmoid_scale = 0.5
-		
-		self.safety = "cf_di" # potential, fdbk_di, cf_di
-		self.rollout_batch_on = True #False # TODO: FIX ME
-		self.kp = 0.01 #0.1
-		self.kv = 2.0  #2.0
+				
+		self.safety = "fdbk_di" # potential, fdbk_di, cf_di
+		self.rollout_batch_on = True
+		self.default_instance = "map_8by8_obst6_agents4_ex0001.yaml"
+		self.current_model = 'il_current.pt'
 
-		# empty good gains: kp=0.005, kv=0.5, kh = 0.5, pi_max: 0.1
+		if self.safety == "fdbk_di":
+			self.pi_max = 0.015 # 0.25
+			self.kp = 0.005 # 0.002
+			self.kv = 0.050 # 0.025
+			self.cbf_kp = 0.5 
+			self.cbf_kd = 2.0
+
+		elif self.safety == "cf_di": # 'working di' parameters
+			self.pi_max = 2.0 # 0.05 
+			self.sigmoid_scale = 5.0 #0.1 # 0.5 
+			self.kp = 0.01 # 0.01 
+			self.kv = 1.0 # 2.0 
+			self.cbf_kp = 0.5 # 0.5
+			self.cbf_kd = 2.0 # 2.0 
+
+		self.Delta_R = 2*self.v_max*self.sim_dt + \
+			(2*self.v_max)**2 / (2 * self.a_max) 
 
 		# obsolete parameters 
 		self.b_gamma = .05 
@@ -126,7 +128,7 @@ class DoubleIntegratorParam(Param):
 		self.vector_plot_dx = 0.25 		
 
 		# self.ad_tf = 25 #25
-		# self.ad_dt = 0.05
+		# self.ad_dt = 0.0
 		# self.ad_times = np.arange(self.sim_t0,self.sim_tf,self.sim_dt)
 
 		# 
@@ -179,7 +181,7 @@ def load_instance(param, env, instance):
 		instance = "map_8by8_obst6_agents16_ex0003.yaml"
 		# instance = "head_test.yaml"
 		# with open("../results/singleintegrator/instances/{}".format(instance)) as map_file:
-		with open("../results/singleintegrator/instances/{}".format(instance)) as map_file:
+		with open("../results/singleintegrator/instances/{}".format(param.default_instance)) as map_file:
 		# test map test dataset
 			map_data = yaml.load(map_file)
 
@@ -240,14 +242,15 @@ if __name__ == '__main__':
 		exit()
 
 	controllers = {
-		'emptywapf': Empty_Net_wAPF(param,env,torch.load('../results/doubleintegrator/exp1Empty_0/il_current.pt')),
-		# 'barrier':torch.load('../results/doubleintegrator/exp1Barrier_0/il_current.pt'),
+		# 'emptywapf': Empty_Net_wAPF(param,env,torch.load('../results/doubleintegrator/exp1Empty_0/il_current.pt')),
+		# 'e2e':torch.load('../results/doubleintegrator/exp1Barrier_0/il_current.pt'),
 		# 'empty':torch.load('../results/doubleintegrator/exp1Empty_0/il_current.pt'),
-		# 
+
+		# 'current':torch.load(param.current_model),
 		# 'current':torch.load(param.il_train_model_fn),
 		# 'current_wapf': Empty_Net_wAPF(param,env,torch.load(param.il_train_model_fn)),
 		# 'gg': GoToGoalPolicy(param,env),
-		# 'apf': Empty_Net_wAPF(param,env,GoToGoalPolicy(param,env)),
+		'apf': Empty_Net_wAPF(param,env,GoToGoalPolicy(param,env)),
 		# 'zero': Empty_Net_wAPF(param,env,ZeroPolicy(env))
 	}
 
