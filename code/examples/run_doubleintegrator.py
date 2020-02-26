9c2df6a0cb42395f6fcff6ec842f8604266e3ba4
bpriviere <benjamin.p.riviere@gmail.com>
2020-02-26 12:01:31 -0800

    di integrator, c code hacks

--- code/examples/run_doubleintegrator.py
+++ code/examples/run_doubleintegrator.py
@@ -5,6 +5,7 @@
 # os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
 from param import Param
+# TEMP
 from run import run, parse_args
 from sim import run_sim
 from systems.doubleintegrator import DoubleIntegrator
@@ -38,7 +39,7 @@ class DoubleIntegratorParam(Param):
 		self.r_agent = 0.15 #0.2
 		self.r_obstacle = 0.5
 		self.v_max = 0.5
-		self.a_max = 2.0 # 7.5
+		self.a_max = 2.0 #.0 #2.0 # 7.5
 		# self.v_max = 100 
 		# self.a_max = 100
 		self.v_min = -1*self.v_max
@@ -46,8 +47,8 @@ class DoubleIntegratorParam(Param):
 
 		# sim 
 		self.sim_t0 = 0
-		self.sim_tf = 100 # 0.1
 		self.sim_dt = 0.05
+		self.sim_tf = 100 #2*self.sim_dt # 0.1
 		self.sim_times = np.arange(self.sim_t0,self.sim_tf,self.sim_dt)
 		self.sim_nt = len(self.sim_times)
 		self.plots_fn = 'plots.pdf'
@@ -57,7 +58,7 @@ class DoubleIntegratorParam(Param):
 				
 		self.safety = "cf_di_2" # potential, fdbk_di, cf_di, cf_di_2
 		self.rollout_batch_on = True
-		self.default_instance = "map_8by8_obst12_agents4_ex0009.yaml"
+		self.default_instance = "map_8by8_obst12_agents8_ex0009.yaml"
 		self.current_model = 'il_current.pt'
 
 		if self.safety == "fdbk_di":
@@ -79,14 +80,11 @@ class DoubleIntegratorParam(Param):
 			self.pi_max = 1.5 # 0.05 
 			self.kp = 0.025 # 0.01 
 			self.kv = 1.0 # 2.0 
-			self.cbf_kp = 0.5 # 0.5
-			self.cbf_kd = 2.0 # 2.0			 
+			self.cbf_kp = 0.035 # 0.5
+			self.cbf_kd = 0.5 # 2.0			 
 
 		self.Delta_R = 2*(0.5*0.05 + 0.5**2/(2*2.0))
-		# self.Delta_R = 2*(self.v_max*self.sim_dt + \
-			# self.v_max**2 / (2 * self.a_max))
-		# self.Delta_R = self.v_max*self.sim_dt + \
-		# 	self.v_max**2 / (2 * self.a_max)
+		self.epsilon = 0.01
 
 		# obsolete parameters 
 		self.b_gamma = .05 
@@ -186,34 +184,68 @@ class DoubleIntegratorParam(Param):
 
 def load_instance(param, env, instance):
 
-	# num_agents = 12
-	# r = 2.0
-	# theta = np.linspace(0, 2*np.pi, num_agents, endpoint=False)
-	# start = np.zeros((num_agents,4))
-	# start[:,0] = r * np.cos(theta)
-	# start[:,1] = r * np.sin(theta)
-	# goal = -start
+	if False:
+		# exp3: mean motion planning 
+		# n_agents = 8
+		# nd2 = int(n_agents/2)
+		# start = np.zeros((n_agents,4))
+		# start[0:nd2,0:2] = np.array([
+		# 	[-2.,0.],
+		# 	[-2.,1.],
+		# 	[-1.,1.],
+		# 	[-1.,-1.2],
+		# 	])
+		# start[nd2:,0:2] = -start[0:nd2,0:2]
+		# goal = -start
+		# obstacles = np.array([
+		# 	[0,0.0],
+		# 	[0,1.75],
+		# 	[0,-1.75],
+		# 	])	
 
-	# # exp3: go to ring from grid
-	# # n_x = 4
-	# # n_y = 3
-	# # l_x = (n_x-1)/4
-	# # l_y = (n_y-1)/4
-	# # temp = np.meshgrid(np.linspace(-l_x,l_x,n_x),np.linspace(-l_y,l_y,n_y))
-	# # start[:,0] = temp[0].flatten()
-	# # start[:,1] = temp[1].flatten()
+		# num_agents = 12
+		# r = 2.0
+		# theta = np.linspace(0, 2*np.pi, num_agents, endpoint=False)
+		# start = np.zeros((num_agents,4))
+		# start[:,0] = r * np.cos(theta)
+		# start[:,1] = r * np.sin(theta)
+		# goal = -start
 
-	# obstacles = np.array([
-	# 	# [0,0.0]
-	# 	])	
+		# # exp3: go to ring from grid
+		# # n_x = 4
+		# # n_y = 3
+		# # l_x = (n_x-1)/4
+		# # l_y = (n_y-1)/4
+		# # temp = np.meshgrid(np.linspace(-l_x,l_x,n_x),np.linspace(-l_y,l_y,n_y))
+		# # start[:,0] = temp[0].flatten()
+		# # start[:,1] = temp[1].flatten()
 
-	# # obstacles -= [0.5,0.5]
-	# InitialState = namedtuple('InitialState', ['start', 'goal'])
-	# s0 = InitialState._make((start.flatten(), goal.flatten()))
-	# param.n_agents = start.shape[0]
-	# env.reset_param(param)
-	# env.obstacles = obstacles
-	# return s0 
+		# obstacles = np.array([
+		# 	# [0,0.0]
+		# 	])	
+
+		# 
+		start = np.array([
+			[4.,4.,0,0],
+			[2,2,0,0],
+			])
+		goal = np.array([
+			[2,2,0,0],
+			[4,4,0,0],
+			],dtype=np.float32)
+		obstacles = np.array([
+			[3., 3.],
+			# [ -0.1, 1.12]
+		])
+
+		obstacles -= [0.5,0.5]
+		InitialState = namedtuple('InitialState', ['start', 'goal'])
+		s0 = InitialState._make((start.flatten(), goal.flatten()))
+		param.n_agents = start.shape[0]
+		env.reset_param(param)
+		env.obstacles = obstacles
+		return s0
+
 
 	import yaml
 	if instance:
@@ -223,8 +255,8 @@ def load_instance(param, env, instance):
 		# default
 		# instance = "map_8by8_obst6_agents64_ex0006.yaml"
 		# instance = "map_8by8_obst6_agents32_ex0005.yaml"
-		instance = "map_8by8_obst6_agents16_ex0003.yaml"
-		# instance = "head_test.yaml"
+		# instance = "map_8by8_obst6_agents16_ex0003.yaml"
+		instance = "head_test.yaml"
 		# with open("../results/singleintegrator/instances/{}".format(instance)) as map_file:
 		with open("../results/singleintegrator/instances/{}".format(param.default_instance)) as map_file:
 		# test map test dataset
@@ -288,13 +320,13 @@ if __name__ == '__main__':
 
 	controllers = {
 		# 'emptywapf': Empty_Net_wAPF(param,env,torch.load('../results/doubleintegrator/exp1Empty_0/il_current.pt')),
-		'e2e':torch.load('../results/doubleintegrator/exp1Barrier_0/il_current.pt'),
+		# 'e2e':torch.load('../results/doubleintegrator/exp1Barrier_0/il_current.pt'),
 		# 'empty':torch.load('../results/doubleintegrator/exp1Empty_0/il_current.pt'),
 
 		# 'current':torch.load(param.il_train_model_fn),
 		# 'current_wapf': Empty_Net_wAPF(param,env,torch.load(param.il_train_model_fn)),
 		# 'gg': GoToGoalPolicy(param,env),
-		# 'apf': Empty_Net_wAPF(param,env,GoToGoalPolicy(param,env)),
+		'apf': Empty_Net_wAPF(param,env,GoToGoalPolicy(param,env)),
 		# 'zero': Empty_Net_wAPF(param,env,ZeroPolicy(env))
 	}
 
